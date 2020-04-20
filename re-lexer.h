b3b7eb50a863e8935bec23d5013467fe7cde6005
Alexei A. Smekalkine <ikle@ikle.ru>
2020-04-21 00:10:05 +0300

    re-lexer: use static functions, simplify

--- re-lexer.h
+++ re-lexer.h
@@ -13,10 +13,34 @@ struct re_lexer {
 	const char *p;
 };
 
-void re_lexer_init (struct re_lexer *o, const char *re);
+static void re_lexer_init (struct re_lexer *o, const char *re)
+{
+	o->p = re;
+}
 
-int re_lexer_peek (struct re_lexer *o);
-int re_lexer_next (struct re_lexer *o);
-int re_lexer_eat  (struct re_lexer *o, int c);
+static int re_lexer_peek (struct re_lexer *o)
+{
+	return *o->p;
+}
+
+static int re_lexer_next (struct re_lexer *o)
+{
+	int c;
+
+	if ((c = re_lexer_peek (o)) == '\0')
+		return c;
+
+	++o->p;
+	return c;
+}
+
+static int re_lexer_eat (struct re_lexer *o, int c)
+{
+	if (re_lexer_peek (o) != c)
+		return 0;
+
+	re_lexer_next (o);
+	return 1;
+}
 
 #endif  /* RE_LEXER_H */
