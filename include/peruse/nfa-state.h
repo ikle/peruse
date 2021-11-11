16e5f4f823c70ac1b7a9473acae012c95a4634ab
Alexei A. Smekalkine <ikle@ikle.ru>
2021-11-12 02:44:04 +0300

    nfa-state: make core API public

---
+++ include/peruse/nfa-state.h
@@ -0,0 +1,39 @@
+/*
+ * Thompson NFA State
+ *
+ * Copyright (c) 2020-2021 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef PERUSE_NFA_STATE_H
+#define PERUSE_NFA_STATE_H  1
+
+#include <stddef.h>
+
+/*
+ * NFA leaf node constructors
+ */
+struct nfa_state *nfa_state_atom  (int c);
+struct nfa_state *nfa_state_range (int from, int to);
+
+/*
+ * NFA compound node constructors
+ */
+struct nfa_state *nfa_state_cat   (struct nfa_state *a, struct nfa_state *b);
+struct nfa_state *nfa_state_union (struct nfa_state *a, struct nfa_state *b);
+struct nfa_state *nfa_state_opt   (struct nfa_state *a);
+struct nfa_state *nfa_state_star  (struct nfa_state *a);
+struct nfa_state *nfa_state_plus  (struct nfa_state *a);
+
+/*
+ * NFA destructor
+ */
+void nfa_state_free (struct nfa_state *o);
+
+/*
+ * Paint NFA root node to a specified color
+ */
+void nfa_state_color (struct nfa_state *o, int color);
+
+#endif  /* PERUSE_NFA_STATE_H */
--- nfa-state.h
+++ nfa-state.h
@@ -1,15 +1,15 @@
 /*
- * Thompson NFA State
+ * Thompson NFA State Internals
  *
  * Copyright (c) 2020-2021 Alexei A. Smekalkine <ikle@ikle.ru>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#ifndef NFA_STATE_H
-#define NFA_STATE_H  1
+#ifndef PERUSE_NFA_STATE_INT_H
+#define PERUSE_NFA_STATE_INT_H  1
 
-#include <stddef.h>
+#include <peruse/nfa-state.h>
 
 enum nfa_type {
 	NFA_SPLIT = -1,
@@ -23,21 +23,8 @@ struct nfa_state {
 	short color;	/* used by lexer to distinguish rules, 1 by default */
 };
 
-struct nfa_state *nfa_state_atom  (int c);
-struct nfa_state *nfa_state_range (int from, int to);
-
-struct nfa_state *nfa_state_cat   (struct nfa_state *a, struct nfa_state *b);
-struct nfa_state *nfa_state_union (struct nfa_state *a, struct nfa_state *b);
-struct nfa_state *nfa_state_opt   (struct nfa_state *a);
-struct nfa_state *nfa_state_star  (struct nfa_state *a);
-struct nfa_state *nfa_state_plus  (struct nfa_state *a);
-
-void nfa_state_free (struct nfa_state *o);
-
 /* set up indexes for NFA state list */
 void nfa_state_order (struct nfa_state *o);
 size_t nfa_state_count (const struct nfa_state *o);
 
-void nfa_state_color (struct nfa_state *o, int color);
-
-#endif  /* NFA_STATE_H */
+#endif  /* PERUSE_NFA_STATE_INT_H */
