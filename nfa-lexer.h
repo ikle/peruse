b3a33949221ea3e1d18ba4fb888b6bad81c2f31b
Alexei A. Smekalkine <ikle@ikle.ru>
2021-11-12 05:25:32 +0300

    nfa-lexer: fully document interface

--- nfa-lexer.h
+++ nfa-lexer.h
@@ -27,17 +27,29 @@ typedef size_t peruse_reader (void *to, size_t count, void *cookie);
  */
 struct nfa_lexer *nfa_lexer_alloc (struct nfa_state *start,
 				   peruse_reader *read, void *cookie);
+/*
+ * The function nfa_lexer_free destroys NFA Lexer context.
+ */
 void nfa_lexer_free (struct nfa_lexer *o);
 
+/*
+ * The function nfa_lexer_eof returns 1 if end of input stream reached
+ * or zero otherwise.
+ */
 int nfa_lexer_eof (struct nfa_lexer *o);
 
+/*
+ * NFA Lexer Token
+ */
 struct nfa_token {
 	char *text;
 	size_t len;
 	int id;
 };
 
-/* returns NULL on error, matched token overwise */
+/*
+ * The function nfa_lexer returns next matched or NULL on error.
+ */
 const struct nfa_token *nfa_lexer (struct nfa_lexer *o);
 
 #endif  /* NFA_LEXER_H */
